feb9b04e7788ad59b9285616b708923a5db17f36
MousaAdnan <106207597+MousaAdnan@users.noreply.github.com>
2023-07-29 02:48:32 +0000

    Fri, Jul 28, 2023, 10:48 PM -04:00

--- mario-more/mario.c
+++ mario-more/mario.c
@@ -1,7 +1,12 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int main(void) 
+int main(void)
 {
-    
+    int height = get_int("How tall should it be? ");
+    int n = 0;
+    while (n < height)
+    {
+        printf("#/n");
+    }
 }
