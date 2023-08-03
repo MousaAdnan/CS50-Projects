9ba48c007535d2088e76d8585d1d9b0cb5c3c980
MousaAdnan <106207597+MousaAdnan@users.noreply.github.com>
2023-08-03 02:46:25 +0000

    Wed, Aug 2, 2023, 10:46 PM -04:00

--- mario-less/mario.c
+++ mario-less/mario.c
@@ -1,7 +1,25 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int main(void) 
+int main(void)
 {
-    
+    int height = get_int("How tall should it be? ");
+    int row = 0;
+    int column;
+    int gap;
+    while (row < height)
+    {
+        printf("#");
+        for (gap = 0; gap < column; gap++)
+        {
+            printf(" ");
+        }
+        for (column = 0; column < row; column++)
+        {
+            printf("#");
+        }
+        printf("\n");
+        row++;
+    }
+
 }
