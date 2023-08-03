3986d4b1ee1e4ae44c89cf77de62887a187918b8
MousaAdnan <106207597+MousaAdnan@users.noreply.github.com>
2023-08-03 02:49:53 +0000

    Wed, Aug 2, 2023, 10:49 PM -04:00

--- mario-less/mario.c
+++ mario-less/mario.c
@@ -10,7 +10,7 @@ int main(void)
     while (row < height)
     {
         printf("#");
-        for (gap = 0; gap < height; gap++)
+        for (gap = 0; gap < (height - row - 1); gap++)
         {
             printf(" ");
         }
