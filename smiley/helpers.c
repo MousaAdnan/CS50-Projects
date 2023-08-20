398b51bc57acb898469990d514f183f80ec9ecb6
MousaAdnan <106207597+MousaAdnan@users.noreply.github.com>
2023-08-20 06:48:35 +0000

    Sun, Aug 20, 2023, 2:48 AM -04:00

--- smiley/helpers.c
+++ smiley/helpers.c
@@ -6,11 +6,11 @@ void colorize(int height, int width, RGBTRIPLE image[height][width])
     {
         for (int h = 0; h < height; h++)
         {
-            if (image[width][height].rgbtBlue == 0 && image[width][height].rgbtRed == 0 && image[width][height].rgbtGreen == 0)
+            if (image[w][h].rgbtBlue == 0 && image[w][h].rgbtRed == 0 && image[w][h].rgbtGreen == 0)
             {
-                image[width][height].rgbtRed = 0;
-                image[width][height].rgbtGreen = 255;
-                image[width][height].rgbtBlue = 0;
+                image[w][h].rgbtRed = 0;
+                image[w][h].rgbtGreen = 255;
+                image[w][h].rgbtBlue = 0;
             }
         }
     }
