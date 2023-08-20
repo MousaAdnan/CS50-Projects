4d6afa5f890ff79d8fe28b1ef353bad55c1a95ba
MousaAdnan <106207597+MousaAdnan@users.noreply.github.com>
2023-08-20 06:44:36 +0000

    Sun, Aug 20, 2023, 2:44 AM -04:00

--- smiley/helpers.c
+++ smiley/helpers.c
@@ -2,5 +2,17 @@
 
 void colorize(int height, int width, RGBTRIPLE image[height][width])
 {
-    // Change all black pixels to a color of your choosing
+    for (int w = 0; w < width; w++)
+    {
+        for (int h = 0; h < height; h++)
+        {
+            if (image[width][height].rgbtBlue == 0 && image[width][height].rgbtRed == 0 && image[width][height].rgbtGreen == 0)
+            {
+                image[width][height].rgbtRed = 0;
+                image[width][height].rgbtGreen = 255;
+                image[width][height].rgbtBlue = 0;
+            }
+        }
+    }
+
 }
