88e61d7ed4461ce8aed03d98654163e7f33b17ab
MousaAdnan <106207597+MousaAdnan@users.noreply.github.com>
2024-07-27 05:45:49 +0000

    Sat, Jul 27, 2024, 1:45 AM -04:00

--- cash/cash.c
+++ cash/cash.c
@@ -3,12 +3,28 @@
 
 int getCents(void);
 int numOfQuarters(cents);
+int numOfDimes(cents);
+int numOfNickels(cents);
+int numOfPennies(cents);
 
 int main(void)
 {
     int cents = getCents();
     int quarters = numOfQuarters(cents);
     cents = cents - quarters * 25;
+
+    int dimes = numOfDimes(cents);
+    cents = cents - dimes * 10;
+
+    int nickels = numOfNickels(cents);
+    cents = cents - nickels*5;
+
+    int pennies = numOfPennies(cents);
+    cents = cents - pennies;
+
+    int coins = dimes + pennies + nickels + quarters;
+
+    printf("%i\n", coins);
 }
 
 int getCents(void)
@@ -24,6 +40,24 @@ int getCents(void)
 
 int numOfQuarters(int cents)
 {
-    int dimes = cents / 10
+    int quarters = cents / 25;
+    return quarters;
+}
+
+int numOfDimes(int cents)
+{
+    int dimes = cents/10;
     return dimes;
+}
+
+int numOfNickels(int cents)
+{
+    int nickels = cents / 5;
+    return nickets;
+}
+
+int numOfPennies(int cents)
+{
+    int pennies = cents;
+    return pennies;
 }
