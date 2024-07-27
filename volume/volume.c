8e812dff9b7a1f6f09b275e5441d6119c1701a03
MousaAdnan <106207597+MousaAdnan@users.noreply.github.com>
2024-07-27 07:11:29 +0000

    Sat, Jul 27, 2024, 3:11 AM -04:00

--- volume/volume.c
+++ volume/volume.c
@@ -34,10 +34,38 @@ int main(int argc, char *argv[])
     float factor = atof(argv[3]);
 
     // TODO: Copy header from input file to output file
+    unsigned char header[HEADER_SIZE];
+    if (fread(header, sizeof(unsigned char), HEADER_SIZE, input) != HEADER_SIZE)
+    {
+        printf("Error reading header from input file.\n");
+        fclose(input);
+        fclose(output);
+        return 1;
+    }
+    fwrite(header, sizeof(unssigned char), HEADER_SIZE, output);
 
     // TODO: Read samples from input file and write updated data to output file
+
+    sample_t sample;
+    while(fread(&sample, sizeof(sample_t), 1, input) == 1)
+    {
+        sample = (sample_t) (sample * factor);
+
+        if (sample > INT16_MAX)
+        {
+            sample = INT16_MAX;
+        }
+        else if (sample < INT16_MIN)
+        {
+            sample = INT16_MIN;
+        }
+
+        fwrite(&sample, sizeof(sample_t), 1, output);
+    }
 
     // Close files
     fclose(input);
     fclose(output);
+
+    return 0;
 }
