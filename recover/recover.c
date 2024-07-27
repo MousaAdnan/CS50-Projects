8a4bdf8b22902e992e7fb19a98270e75eddcfed9
MousaAdnan <106207597+MousaAdnan@users.noreply.github.com>
2024-07-27 07:40:01 +0000

    Sat, Jul 27, 2024, 3:40 AM -04:00

--- recover/recover.c
+++ recover/recover.c
@@ -2,7 +2,11 @@
 #include <stdlib.h>
 
 #define BLOCK_SIZE 512
-#
+#define JPEG_SIGNATURE_1 0xff
+#define JPEG_SIGNATURE_2 0xd8
+#define JPEG_SIGNATURE_3 0xff
+#define JPEG_SIGNATURE_4_START 0xe0
+#define JPEG_SIGNATURE_4_END 0xef
 
 int main(int argc, char *argv[])
 {
