8b1aaae1557279c2a8fc9b8de7cbf577394f1218
MousaAdnan <106207597+MousaAdnan@users.noreply.github.com>
2024-07-27 07:12:48 +0000

    Sat, Jul 27, 2024, 3:12 AM -04:00

--- volume/volume.c
+++ volume/volume.c
@@ -42,7 +42,7 @@ int main(int argc, char *argv[])
         fclose(output);
         return 1;
     }
-    fwrite(header, sizeof(unssigned char), HEADER_SIZE, output);
+    fwrite(header, sizeof(unsigned char), HEADER_SIZE, output);
 
     // TODO: Read samples from input file and write updated data to output file
 
