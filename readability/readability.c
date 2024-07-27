128d6893d1e7b5173bd647fae9d81b6cb3e0a27e
MousaAdnan <106207597+MousaAdnan@users.noreply.github.com>
2024-07-27 06:49:16 +0000

    Sat, Jul 27, 2024, 2:49 AM -04:00

---
+++ readability/readability.c
@@ -0,0 +1,62 @@
+#include <cs50.h>
+#include <math.h>
+#include <ctype.h>
+#include <stdio.h>
+
+int main(void)
+{
+    string text = get_string("Text: ");
+    int letters = 0;
+    int words = 0;
+    int sentences = 0;
+    char c;
+    bool inWord = false;
+
+    for (int n = 0; text[n] != '\0'; n++)
+    {
+        c = text[n];
+
+        if (isalpha(c))
+        {
+            letters++;
+        }
+
+        if(isspace(c) || c == '\n')
+        {
+            if (inWord)
+            {
+                words++;
+                inWord = false;
+            }
+        }
+        else
+        {
+            inWord = true;
+        }
+
+        if (c == '.' || c == '!' || c == '?')
+        {
+            sentences++;
+        }
+    }
+
+    double L = (double) letters/ words * 100;
+    double S = (double) sentences / words * 100;
+
+    double index = 0.0588 * L - 0.296 * S - 15.8;
+    int grade = (int) round (index);
+
+    if (grade >= 16)
+    {
+        printf("Grade 16+\n");
+    }
+    else if (grade < 1)
+    {
+        printf("Before Grade 1\n");
+    }
+    else{
+        printf("Grade %d\n", grade);
+    }
+
+    return 0;
+}
