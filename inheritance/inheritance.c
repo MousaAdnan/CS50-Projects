4bbcfdeb2316819c714b18ec1df28bf88b0a8798
MousaAdnan <106207597+MousaAdnan@users.noreply.github.com>
2024-07-28 07:39:28 +0000

    Sun, Jul 28, 2024, 3:39 AM -04:00

--- inheritance/inheritance.c
+++ inheritance/inheritance.c
@@ -50,9 +50,10 @@ person *create_family(int generations)
 
         // TODO: Set parent pointers for current person
         p->parents[0] = parent0;
-        p->parents[1] = parents1;
+        p->parents[1] = parent1;
 
         // TODO: Randomly assign current person's alleles based on the alleles of their parents
+        
     }
 
     // If there are no generations left to create
