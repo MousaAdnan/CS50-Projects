8eabbf6d3d09c23d9867fd570c2c8049fa129d77
MousaAdnan <106207597+MousaAdnan@users.noreply.github.com>
2024-07-28 10:04:43 +0000

    Sun, Jul 28, 2024, 6:04 AM -04:00

--- movies/11.sql
+++ movies/11.sql
@@ -2,4 +2,7 @@ SELECT movies.title
 FROM movies
 JOIN stars ON movies.id = stars.movie_id
 JOIN people ON stars.person_id = people.id
-JOIN ratingss ON MOVIES.
+JOIN ratings ON movies.id = ratings.movie_id
+WHERE people.name = 'Chadwich Boseman'
+ORDER BY ratings.rating DESC
+LIMIT 5;
