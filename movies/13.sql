d1b2b11cebccd8edefca661f2dc8b0dc9af753e2
MousaAdnan <106207597+MousaAdnan@users.noreply.github.com>
2024-07-28 10:09:20 +0000

    Sun, Jul 28, 2024, 6:09 AM -04:00

--- movies/13.sql
+++ movies/13.sql
@@ -1,6 +1,6 @@
 SELECT DISTINCT p2.name
 FROM people AS p1
-JOIN stars AS s1 ON p1.id = s1.movie_id
+JOIN stars AS s1 ON p1.id = s1.person_id
 JOIN movies AS m1 ON s1.movie_id = m1.id
 JOIN stars AS s2 ON m1.id = s2.movie_id
 JOIN people AS p2 ON s2.person_id = p2.id
