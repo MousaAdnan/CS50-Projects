48c691ac6e23aaebe9218eb987e326e7ca375313
MousaAdnan <106207597+MousaAdnan@users.noreply.github.com>
2024-07-30 04:52:19 +0000

    Tue, Jul 30, 2024, 12:52 AM -04:00

--- finance/app.py
+++ finance/app.py
@@ -136,9 +136,11 @@ def register():
         if len(rows) != 0:
             return apology("username already exists", 400)
 
-        hashcode = generate_password_hash(password, method='pbkdf2:sha256', salt_length=8)
-        db.execute("INSERT INTO users (username, hash) VALUES(?, ?)", username, hashcode)
+        db.execute("INSERT INTO users (username, hash) VALUES(?, ?)", request.form.get("username"), generate_password_hash(request.form.get("username"))
 
+        rows = db.execute("SELECT * FROM users WHERE username = ?", request.form.get("username"))
+
+        session["user_id"] = rows[0]["id"]
         return redirect("/")
 
     else:
